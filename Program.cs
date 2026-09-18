6188a67b20f36af0bb50952042a978b07ef9942a
Vinicius091 <viniferr2006@gmail.com>
2026-09-18 18:23:11 -0300

    escreve em baleiês

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        string texto = Console.ReadLine();
+
+        foreach (char letra in texto)
+        {
+            if (letra == 'a')
+            {
+                Console.Write("aaa");
+            }
+            else if (letra == 'e')
+            {
+                Console.Write("eee");
+            }
+            else if (letra == 'i')
+            {
+                Console.Write("iii");
+            }
+            else if (letra == 'o')
+            {
+                Console.Write("ooo");
+            }
+            else if (letra == 'u')
+            {
+                Console.Write("uuu");
+            }
+            else
+            {
+                Console.Write(letra);
+            }
+        }
+    }
+}
