fd6620315a7ac0fd94aeb1350ad44373799f7e8d
Vinicius091 <viniferr2006@gmail.com>
2026-09-25 18:08:20 -0300

    Escreve as palavras em baleiês

--- Program.cs
+++ Program.cs
@@ -1,37 +1,35 @@
 ﻿using System;
 
-class Program
-{
-    static void Main()
-    {
-        string texto = Console.ReadLine();
 
-        foreach (char letra in texto)
-        {
-            if (letra == 'a')
-            {
-                Console.Write("aaa");
-            }
-            else if (letra == 'e')
-            {
-                Console.Write("eee");
-            }
-            else if (letra == 'i')
-            {
-                Console.Write("iii");
-            }
-            else if (letra == 'o')
-            {
-                Console.Write("ooo");
-            }
-            else if (letra == 'u')
-            {
-                Console.Write("uuu");
-            }
-            else
-            {
-                Console.Write(letra);
-            }
-        }
+Console.WriteLine("Digite um texto:");
+
+string texto = Console.ReadLine() ?? string.Empty;
+Console.Write("Em baleiês: ");
+
+foreach (char letra in texto)
+{
+    if (letra == 'a')
+    {
+        Console.Write("aaa");
+    }
+    else if (letra == 'e')
+    {
+        Console.Write("eee");
+    }
+    else if (letra == 'i')
+    {
+        Console.Write("iii");
+    }
+    else if (letra == 'o')
+    {
+        Console.Write("ooo");
+    }
+    else if (letra == 'u')
+    {
+        Console.Write("uuu");
+    }
+    else
+    {
+        Console.Write(letra);
     }
 }
